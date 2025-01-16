fc553dd759e22c7ccaeabc31d60e69de8ffd7697
SamruthDS <samruthvadada@gmail.com>
2025-01-16 22:01:29 +0530

    Update Task-03.R

--- Task-03.R
+++ Task-03.R
@@ -16,23 +16,23 @@ bank_data <- read.csv("C:/Users/ASUS/Downloads/bank-full.csv", sep = ";")  # Upd
 str(bank_data)
 
 # 1. Data Preprocessing
-# Convert the target variable ('y') to a factor
+# Converting the target variable ('y') to a factor
 bank_data$y <- as.factor(bank_data$y)
 
-# Split the data into training and testing sets
+# Spliting the data into training and testing sets
 set.seed(123)  # For reproducibility
 train_index <- createDataPartition(bank_data$y, p = 0.8, list = FALSE)
 train_data <- bank_data[train_index, ]
 test_data <- bank_data[-train_index, ]
 
 # 2. Build the Decision Tree Classifier
-# Train the decision tree model
+# Training the decision tree model
 decision_tree <- rpart(y ~ ., data = train_data, method = "class")
 
-# Visualize the decision tree
+# Visualizing the decision tree
 rpart.plot(decision_tree, main = "Decision Tree for Bank Marketing Data")
 
-# 3. Evaluate the Model
+# 3. Evaluating the Model
 # Make predictions on the test data
 predictions <- predict(decision_tree, test_data, type = "class")
 
